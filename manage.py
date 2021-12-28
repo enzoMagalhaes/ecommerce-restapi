4fdb09d0d60835c2ad8313e81b0680e4def1482c
enzom02 <enzom2852002@gmail.com>
2021-12-27 21:51:00 -0300

    dotenv error

--- manage.py
+++ manage.py
@@ -2,12 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import dotenv
 
 def main():
 
     # set enviroment variables in development
     try:
+        import dotenv
         dotenv.read_dotenv()
     except Exception as e:
         pass
